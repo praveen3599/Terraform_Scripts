8117ba5777ae078cd2fa0f4f6a2e7d958062e391
praveen3599 <praveennemalikanti67@gmail.com>
2025-07-15 10:20:30 -0400

    added the terraform script eithout secrets

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,6 +1,13 @@
-
+provider "aws"{
+    region = "us-east-1"
+    access_key=var.aws_access_key
+    secret_key=var.aws_secret_key
+}
 
 resource "aws_instance" "myec2"{
     ami = "ami-0150ccaf51ab55a51"
     instance_type= "t2.micro"
+    tags = {
+      "Name"= "Testing-EC2"
+    }
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
+variable "aws_access_key"{
+    description = "aws access key"
+    type= string
 }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
+variable "aws_secret_key"{
+    description = "aws access key"
+    type= string
 }
